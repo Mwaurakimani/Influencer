2265123156c6e1e55a441f887b2389768e6dfe14
MwauraKimani <kimmwaus@gmail.com>
2023-02-25 22:59:38 +0300

    build front end

---
+++ public/build/assets/CreateInfluencer-3aafa77f.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";const r={};function n(c,t){return null}const f=e(r,[["render",n]]);export{f as default};
---
+++ public/build/assets/Finance-6a9a4bbc.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./_plugin-vue_export-helper-c27b6911.js";const c={};function e(r,t){return null}const o=n(c,[["render",e]]);export{o as default};
