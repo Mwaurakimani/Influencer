1a4b17ad4d731c770d891b9901bfeaf2f3061643
Ubuntu <ubuntu@ip-172-31-47-53.eu-west-2.compute.internal>
2023-06-23 22:08:17 +0000

    update one

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit169cb8a7e64bf81ee8a0ea277756ee54
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit169cb8a7e64bf81ee8a0ea277756ee54::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire169cb8a7e64bf81ee8a0ea277756ee54($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit169cb8a7e64bf81ee8a0ea277756ee54::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire169cb8a7e64bf81ee8a0ea277756ee54($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
